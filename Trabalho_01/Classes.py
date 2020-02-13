9f8171faa5e4b0de346b0c412c11f1396e32eb63
Alunos <Alunos>
2020-02-13 20:04:08 -0300

    Modificado em 12/02/2020

---
+++ Trabalho_01/Classes.py
@@ -0,0 +1,60 @@
+class Ponto_1:
+
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+
+    #Getters
+    def getX(self):
+        self._x
+
+
+    def getY(self):
+        self._x
+
+
+    #Setters
+    def set_x(self, x):
+        self._x = x
+
+
+    def set_y(self, y):
+        self._y = y
+
+
+    def mostra_ponto1(self):
+        print('X: ', self._x, 'Y: ', self._y)
+
+
+class Pontos_p1_p2:
+
+
+    def __init__(self, p1, p2):
+        self._p1 = p1
+        self._p2 = p2
+
+
+    #Getters
+    def getX(self):
+        self._p1
+
+
+    def getY(self):
+        self._p2
+
+
+    #Setters
+    def set_x(self, p1):
+        self._p1 = p1
+
+
+    def set_y(self, p2):
+        self._p2 = p2
+
+
+    def mostra_ponto1(self):
+        print('X: ', self._p1, 'Y: ', self._p2)
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from Trabalho_01.Classes import *
+from pylab import plot, show, plt
+
+
+ponto1 = Ponto_1(2, 4)
+#ponto2 = Pontos_p1_p2(2, 4)
+
+
+plt.plot((1, 2, 3, 4, 5), (1, 2, 3, 4, 5), 'ro-')
+
+plt.axis((0, 6, 1 , 10))
+
+show()
+
