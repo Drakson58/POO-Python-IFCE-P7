bde5bcc2ec31e41a62f7ae94e54b7dfa52896489
Alunos <Alunos>
2020-02-14 00:12:52 -0300

    Atualizado em 13/02/2020 LMC-3

--- Trabalho_01/Classes.py
+++ Trabalho_01/Classes.py
@@ -28,6 +28,13 @@ class Ponto_1:
         print('X: ', self._x, 'Y: ', self._y)
 
 
+    def separando_pontos(self):
+        x1 = self._x
+        x2 = self._y
+        pontos = [x1, x2]
+        return pontos
+
+
 class Pontos_p1_p2:
 
 
@@ -56,5 +63,12 @@ class Pontos_p1_p2:
 
     def mostra_ponto1(self):
         print('X: ', self._p1, 'Y: ', self._p2)
+
+
+    def separando_pontos(self):
+        p1 = self._p1
+        p2 = self._p2
+        pontos = [p1, p2]
+        return pontos
 
 
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 from Trabalho_01.Classes import *
-from pylab import plot, show, plt
+from pylab import show, plt
 
 
-ponto1 = Ponto_1(2, 4)
-#ponto2 = Pontos_p1_p2(2, 4)
+
+def ler_num(msg):
+
+    while True:
+
+        try:
+
+            num = int(input(msg)) or float(input(msg))
+            return num
+        except ValueError:
+
+            print('ERRO! Digite um número inteiro ou flotoante.')
+            continue
+
+print('Valores do ponto X.')
+x2 = ler_num('Digite X1: ')
+x1 = ler_num('Digite X2: ')
 
 
-plt.plot((1, 2, 3, 4, 5), (1, 2, 3, 4, 5), 'ro-')
+print('Valores do ponto P')
+#y1 = ler_num('Digite Y1: ')
+#y2 = ler_num('DIgite y2:')
 
-plt.axis((0, 6, 1 , 10))
+
+
+ponto1 = Ponto_1(x1, x2)
+pontos = ponto1.separando_pontos()
+
+ponto2 = Pontos_p1_p2(2, 4)
+pontosP = ponto2.separando_pontos()
+
+
+
+
+                 #y1 y2                   #p1 p2
+plt.plot((pontos[0], pontos[1]), (pontosP[0], pontosP[1]), 'bo-')
+
+
+#Tamanho do plano cartesiano
+plt.axis((0, 10, 1, 10))
 
 show()
 
