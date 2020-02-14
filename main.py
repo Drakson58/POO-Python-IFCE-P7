d4f9753d094850ccf0e17cc29917b167d62c60c7
Alunos <Alunos>
2020-02-14 16:50:30 -0300

    Quadrantes desenhados na tela

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ print(' --  VALORES DO PONTO 2 -- ')
 x2 = ler_num('Digite o valor de X2: ')
 y2 = ler_num('Digite o valor de Y2: ')
 
-
 #Passando dados para as classes.
 ponto1 = Ponto_1(x1, y1)
 pontosX = ponto1.separando_pontos()
@@ -35,7 +34,15 @@ pontosX = ponto1.separando_pontos()
 ponto2 = Pontos_2(x2, y2)
 pontosP = ponto2.separando_pontos()
 
-
+#Linhas que forma os quadrantes
+plt.hold('on')
+# Set x-axis range
+plt.xlim((0,10))
+# Set y-axis range
+plt.ylim((1,10))
+# Draw lines to split quadrants
+plt.plot([5,5],[1,9], linewidth=4, color='red' )
+plt.plot([1,9],[5,5], linewidth=4, color='red' )
 
 
 #Passando os valores dos pontos para o plano cartesiano.
