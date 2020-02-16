e0c3e08fdf0c7582eb7a264fa2f543d0b8926389
Drakson58 <darksonbezerra123@gmail.com>
2020-02-15 21:21:55 -0300

    Trabalho atualizado

--- Trabalho_01/Classes.py
+++ Trabalho_01/Classes.py
@@ -1,5 +1,5 @@
-class Ponto_1:
-
+class X_e_Y:
+# Está classe recebe dois pontos no plano cartesiano.
 
     def __init__(self, x, y):
         self._x = x
@@ -8,34 +8,23 @@ class Ponto_1:
 
     #Getters
     def getX(self):
-        self._x
+        return self._x
 
 
     def getY(self):
-        self._x
+        return self._y
 
 
-    #Setters
-    def set_x(self, x):
+    #Seterrs
+    def setX(self, x):
         self._x = x
 
 
-    def set_y(self, y):
+    def setY(self, y):
         self._y = y
 
 
-    def mostra_ponto1(self):
-        print('X: ', self._x, 'Y: ', self._y)
-
-
-    def separando_pontos(self):
-        x1 = self._x
-        x2 = self._y
-        pontos = [x1, x2]
-        return pontos
-
-
-class Pontos_2:
+class Reta(X_e_Y):
 
 
     def __init__(self, p1, p2):
@@ -43,32 +32,52 @@ class Pontos_2:
         self._p2 = p2
 
 
-    #Getters
-    def getX(self):
-        self._p1
+    #Geterrs
+    def getP1(self):
+        return self._p1
 
 
-    def getY(self):
-        self._p2
+    def getP2(self):
+        return self._p2
 
 
     #Setters
-    def set_x(self, p1):
+    def setP1(self, p1):
         self._p1 = p1
 
 
-    def set_y(self, p2):
+    def setP2(self, p2):
         self._p2 = p2
 
 
-    def mostra_ponto1(self):
-        print('X: ', self._p1, 'Y: ', self._p2)
+    def mostra_pontos(self):
+        print(self._p1, self._p2)
 
 
-    def separando_pontos(self):
-        p1 = self._p1
-        p2 = self._p2
-        pontos = [p1, p2]
-        return pontos
+    #Retornando os valores separados de cada ponto. (Retorna para o plano cartesiano)
+    def retornar_p1X(self):
+        return self._p1[0]
+    def retornar_p1Y(self):
+        return self._p1[1]
 
 
+    def retornar_p2X(self):
+        return self._p2[0]
+    def retornar_p2Y(self):
+        return self._p2[1]
+
+
+
+#Função para testar se é um número.
+def ler_num(msg):
+
+    while True:
+
+        try:
+
+            num = float(input(msg))
+            if num < 21 and num > -21:
+                return num
+        except ValueError:
+
+            print('ERRO! Digite um número inteiro ou flotoante.')
--- main.py
+++ main.py
@@ -2,21 +2,6 @@ from Trabalho_01.Classes import *
 from pylab import show, plt
 
 
-
-def ler_num(msg):
-
-    while True:
-
-        try:
-
-            num = float(input(msg))
-            return num
-        except ValueError:
-
-            print('ERRO! Digite um número inteiro ou flotoante.')
-            continue
-
-
 #Recebendo os dados da Classe 'Ponto_1'  (X, Y)
 print(' --  VALORES DO PONTO 1 -- ')
 x1 = ler_num('Digite o valor de X1: ')
@@ -27,22 +12,37 @@ print(' --  VALORES DO PONTO 2 -- ')
 x2 = ler_num('Digite o valor de X2: ')
 y2 = ler_num('Digite o valor de Y2: ')
 
+#Colocando os valores em uma lista.
+p1 = [x1, y1]
+p2 = [x2, y2]
+
+#Passando para a classe reta.
+reta = Reta(p1, p2)
+#reta.mostra_pontos()
+reta.retornar_p1X()
+
+
+
+'''
 #Passando dados para as classes.
 ponto1 = Ponto_1(x1, y1)
 pontosX = ponto1.separando_pontos()
 
 ponto2 = Pontos_2(x2, y2)
 pontosP = ponto2.separando_pontos()
+'''
 
-#Linhas que forma os quadrantes
 
+#Codigo que forma o quadrante.
 # Tamanho das linhas
+
+        # Reta vertical
 plt.plot([0, 0],[20, -20], linewidth=4, color='red' )
 plt.plot([20, -20],[0, 0], linewidth=4, color='red' )
         # Reta horizontal
 
 #Passando os valores dos pontos para o plano cartesiano.
-plt.plot((y1, y2), (x1, x2), 'bo-')
+plt.plot((reta.retornar_p1X(), reta.retornar_p2X()), (reta.retornar_p1Y(), reta.retornar_p2Y()), 'bo-')
 
 #Tamanho do plano cartesiano.
 plt.axis((-20, 20, -20, 20))
